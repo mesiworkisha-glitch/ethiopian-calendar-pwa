819e466129b68e4406e444de2100b220435b03a8
mesiworkisha-glitch <mesi.workisha@gmail.com>
2026-09-09 20:44:04 +0300

    Point browser tests to smoke test filename

--- planning.playwright.config.js
+++ planning.playwright.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('@playwright/test');
 
 module.exports = defineConfig({
   testDir: '.',
-  testMatch: 'planning.browser.test.js',
+  testMatch: 'planning-browser-smoke.js',
   use: {
     baseURL: 'http://127.0.0.1:4173',
     headless: true,
